fb6db39d0ed2af92b3617ca968e7929cc5748e9a
Alexios <goddel@gmail.com>
2016-01-11 00:11:43 +0100

    custom shapes  full update

--- src/main/java/graphicsUI/CargoSetWeightsPanel.java
+++ src/main/java/graphicsUI/CargoSetWeightsPanel.java
@@ -19,22 +19,43 @@ public class CargoSetWeightsPanel extends JPanel {
 	private ImageIcon imageA = new ImageIcon("Textures/DefaultA.jpg");
 	private ImageIcon imageB = new ImageIcon("Textures/DefaultB.jpg");
 	private ImageIcon imageC = new ImageIcon("Textures/DefaultC.jpg");
-	private JLabel cargoALabel= new  JLabel("Cargo A weight");
-	private JLabel cargoBLabel= new  JLabel("Cargo B weight");
-	private JLabel cargoCLabel= new  JLabel("Cargo C weight");
+	private JLabel cargoALabel = new  JLabel("Cargo A weight");
+	private JLabel cargoBLabel = new  JLabel("Cargo B weight");
+	private JLabel cargoCLabel = new  JLabel("Cargo C weight");
 	private JButton setWeightsButton = new JButton("Set Weights");
-	private JButton  setShapeA= new JButton("set cargo A");
-	private JTextField weightCargoA;
-	private JTextField weightCargoB;
-	private JTextField weightCargoC;
-	private int 	y;
-	private int 	x ;
-	private int 	z ;
+	private JButton  setShapeA = new JButton("set cargo A");
+	private JButton  setShapeB = new JButton("set cargo B");
+	private JButton  setShapeC = new JButton("set cargo C");
+	private JTextField weightCargoA = new JTextField();;
+	private JTextField weightCargoB= new JTextField();;
+	private JTextField weightCargoC= new JTextField();;
+	private int yCargoA;
+	private int xCargoA;
+	private int zCargoA;
+	private int yCargoB;
+	private int xCargoB;
+	private int zCargoB;
+	private int yCargoC;
+	private int xCargoC;
+	private int zCargoC;
 	private JTextField shapeAY = new JTextField();
 	private JTextField shapeAX = new JTextField();
 	private	JTextField shapeAZ = new JTextField();
-	
-	
+	private JTextField shapeBY = new JTextField();
+	private JTextField shapeBX = new JTextField();
+	private	JTextField shapeBZ = new JTextField();
+	private JTextField shapeCY = new JTextField();
+	private JTextField shapeCX = new JTextField();
+	private	JTextField shapeCZ = new JTextField();
+	private JLabel shapeAYlabel= new  JLabel("Xcoord");
+	private JLabel shapeAXlabel= new  JLabel("Y coord");
+	private JLabel shapeAZlabel= new  JLabel("Z coord");
+	private JLabel shapeBYlabel= new  JLabel("Xcoord");
+	private JLabel shapeBXlabel= new  JLabel("Y coord");
+	private JLabel shapeBZlabel= new  JLabel("Z coord");
+	private JLabel shapeCYlabel= new  JLabel("Xcoord");
+	private JLabel shapeCXlabel= new  JLabel("Y coord");
+	private JLabel shapeCZlabel= new  JLabel("Z coord");
 	private boolean shapesSet= false;
 	private RunTimeData runtimeData;
 	
@@ -51,23 +72,25 @@ if(shapesSet){
 		g.drawImage(imageB.getImage(), 200, 0, 125, 125, null);
 		g.drawImage(imageC.getImage(), 325, 0, 125, 125, null);
 }
-setCustomShapeMenu();
-//	setWeightFields();
+setCustomCargoA(0,0,setShapeA);
+setCustomCargoB(140,0,setShapeB);
+setCustomCargoC(280,0,setShapeC);
+	setWeightFields();
 	}
 	private void setWeightFields() {
-		weightCargoA = new JTextField();
+		
 		weightCargoA.setBounds(115, 130, 50, 20);
 		cargoALabel.setBounds(90, 152, 120, 15);
 		add(cargoALabel);
 		add(weightCargoA);
 
-		weightCargoB = new JTextField();
+		
 		weightCargoB.setBounds(240, 130, 50, 20);
 		cargoBLabel.setBounds(215, 152, 120, 15);
 		add(weightCargoB);
 		add(cargoBLabel);
 
-		weightCargoC = new JTextField();
+	
 		weightCargoC.setBounds(365, 130, 50, 20);
 		cargoCLabel.setBounds(340, 152, 120, 15);
 		add(weightCargoC);
@@ -78,41 +101,78 @@ setCustomShapeMenu();
 		setWeightsButton.setBounds(500, 125, 160, 30);
 		add(setWeightsButton);
 	}
-	private void setCustomShapeMenu() {
-		
-		JLabel shapeNamelabel= new  JLabel("Cargo A dimentions");
-		shapeNamelabel.setBounds(235, 45, 120, 15);
-		add(shapeNamelabel);
-		
-		
-		shapeAY.setBounds(75, 40, 50, 20);
-		JLabel shapeAYlabel= new  JLabel("Xcoord");
-		shapeAYlabel.setBounds(75, 65, 120, 15);
+
+	private void setCustomCargoA(int x, int y, JButton aButton) {
+
+		shapeAY.setBounds(75 + x, 40 + y, 50, 20);
+
+		shapeAYlabel.setBounds(130 + x, 40 + y, 120, 15);
 		add(shapeAY);
 		add(shapeAYlabel);
 
-		
-		
-		shapeAX.setBounds(125,40, 50, 20);
-		JLabel shapeAXlabel= new  JLabel("Y coord");
-		shapeAXlabel.setBounds(125, 65, 120, 15);
+		shapeAX.setBounds(75 + x, 60 + y, 50, 20);
+
+		shapeAXlabel.setBounds(130 + x, 60 + y, 120, 15);
 		add(shapeAX);
 		add(shapeAXlabel);
 
-		
-		
-		shapeAZ.setBounds(175,40, 50, 20);
-		JLabel shapeAZlabel= new  JLabel("Zxcoord");
-		shapeAZlabel.setBounds(175, 65, 120, 15);
+		shapeAZ.setBounds(75 + x, 80 + y, 50, 20);
+
+		shapeAZlabel.setBounds(130 + x, 80 + y, 120, 15);
 		add(shapeAZ);
 		add(shapeAZlabel);
-		
-		
-		 
-		
-		setShapeA.addActionListener(new SetShapeSetButtonListener());
-		setShapeA.setBounds(375, 45, 120, 15);
-		add(setShapeA);
+
+		aButton.addActionListener(new SetCargoAButton());
+		aButton.setBounds(75 + x, 100 + y, 120, 15);
+		add(aButton);
+	}
+	private void setCustomCargoB(int x, int y, JButton aButton) {
+
+		shapeBY.setBounds(75 + x, 40 + y, 50, 20);
+
+		shapeBYlabel.setBounds(130 + x, 40 + y, 120, 15);
+		add(shapeBY);
+		add(shapeBYlabel);
+
+		shapeBX.setBounds(75 + x, 60 + y, 50, 20);
+
+		shapeBXlabel.setBounds(130 + x, 60 + y, 120, 15);
+		add(shapeBX);
+		add(shapeBXlabel);
+
+		shapeBZ.setBounds(75 + x, 80 + y, 50, 20);
+
+		shapeBZlabel.setBounds(130 + x, 80 + y, 120, 15);
+		add(shapeBZ);
+		add(shapeBZlabel);
+
+		aButton.addActionListener(new SetCargoBButton());
+		aButton.setBounds(75 + x, 100 + y, 120, 15);
+		add(aButton);
+	}
+	private void setCustomCargoC(int x, int y, JButton aButton) {
+
+		shapeCY.setBounds(75 + x, 40 + y, 50, 20);
+
+		shapeCYlabel.setBounds(130 + x, 40 + y, 120, 15);
+		add(shapeCY);
+		add(shapeCYlabel);
+
+		shapeCX.setBounds(75 + x, 60 + y, 50, 20);
+
+		shapeCXlabel.setBounds(130 + x, 60 + y, 120, 15);
+		add(shapeCX);
+		add(shapeCXlabel);
+
+		shapeCZ.setBounds(75 + x, 80 + y, 50, 20);
+
+		shapeCZlabel.setBounds(130 + x, 80 + y, 120, 15);
+		add(shapeCZ);
+		add(shapeCZlabel);
+
+		aButton.addActionListener(new SetCargoCButton());
+		aButton.setBounds(75 + x, 100 + y, 120, 15);
+		add(aButton);
 	}
 	class WeightButtonListener implements ActionListener {
 
@@ -123,20 +183,54 @@ setCustomShapeMenu();
 				
 			}
 		}
-	class SetShapeSetButtonListener implements ActionListener {
+	class SetCargoAButton implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Cargos were set");
 			if (shapeAX.getText().length() > 0 && shapeAY.getText().length() > 0 && shapeAZ.getText().length() > 0) {
-			y = Integer.parseInt(shapeAY.getText());
-				x = Integer.parseInt(shapeAX.getText());
-			z = Integer.parseInt(shapeAZ.getText());
+			yCargoA = Integer.parseInt(shapeAY.getText());
+			xCargoA = Integer.parseInt(shapeAX.getText());
+			zCargoA = Integer.parseInt(shapeAZ.getText());
 				
 						
 				}
 			runtimeData.getCargoData().getShapeList().remove(0);
-			runtimeData.getCargoData().getShapeList().add(0,new CargoGenerator(y, x, z, 1));
+			runtimeData.getCargoData().getShapeList().add(0,new CargoGenerator(yCargoA, xCargoA, zCargoA, 1));
+				
+			}
+		}
+	class SetCargoBButton implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Cargos were set");
+			if (shapeBX.getText().length() > 0 && shapeBY.getText().length() > 0 && shapeBZ.getText().length() > 0) {
+			yCargoB = Integer.parseInt(shapeBY.getText());
+			xCargoB = Integer.parseInt(shapeBX.getText());
+			zCargoB = Integer.parseInt(shapeBZ.getText());
+				
+						
+				}
+			runtimeData.getCargoData().getShapeList().remove(1);
+			runtimeData.getCargoData().getShapeList().add(1,new CargoGenerator(yCargoB, xCargoB, zCargoB, 1));
+				
+			}
+		}
+	class SetCargoCButton implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Cargos were set");
+			if (shapeCX.getText().length() > 0 && shapeCY.getText().length() > 0 && shapeCZ.getText().length() > 0) {
+			yCargoC = Integer.parseInt(shapeCY.getText());
+			xCargoC = Integer.parseInt(shapeCX.getText());
+			zCargoC = Integer.parseInt(shapeCZ.getText());
+				
+						
+				}
+			runtimeData.getCargoData().getShapeList().remove(2);
+			runtimeData.getCargoData().getShapeList().add(2,new CargoGenerator(yCargoC, xCargoC, zCargoC, 1));
 				
 			}
 		}
