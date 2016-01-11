4e2d8ff63fa672b6573efedfc477a5529e0ef653
Alexios <goddel@gmail.com>
2016-01-11 11:53:07 +0100

    d

--- src/main/java/graphicsUI/UIWindow.java
+++ src/main/java/graphicsUI/UIWindow.java
@@ -1,6 +1,7 @@
 package graphicsUI;
 
 import java.awt.BorderLayout;
+import java.util.Timer;
 
 import javax.swing.JFrame;
 
@@ -9,31 +10,41 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UIWindow extends JFrame{
-	
+public class UIWindow extends JFrame {
+	Timer refreshTimer = new Timer();
 	private RunTimeData runtimeData;
-	
+
 	public UIWindow(RunTimeData runtimeData) {
-		this.runtimeData=runtimeData;
+		this.runtimeData = runtimeData;
 
 		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Cargo Loader");
 		setResizable(false);
 
-		
 		add(new CargoSpaceMenu(this.runtimeData), BorderLayout.SOUTH);
 		add(new RunAlgorithmsMenu(this.runtimeData), BorderLayout.WEST);
-		CargoSetWeightsPanel centerPanel = new CargoSetWeightsPanel(this.runtimeData);
+		final CargoSetWeightsPanel centerPanel = new CargoSetWeightsPanel(this.runtimeData);
 		add(centerPanel, BorderLayout.CENTER);
 		add(new CargoSetsMenu(this.runtimeData, centerPanel), BorderLayout.NORTH);
 
 		setVisible(true);
 
-	}
-	
-	
-	
-	
+		// Timer refreshTimer = new Timer();
+		// TimerTask refreshRate = new TimerTask() {
+		//
+		// @Override
+		// public void run() {
+		// centerPanel.repaint();
+		//
+		// }
+		// };
+		// refreshTimer.schedule(refreshRate, 0, 16);
+		//
+		//
 
 }
+}
+	
+
+
